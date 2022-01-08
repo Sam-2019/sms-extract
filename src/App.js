d3dfa88e63e985676ed3d723860650106a5f4f99
Samuel Martey <sam@encodevlabs.com>
2022-01-08 16:41:15 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { check } from "./smsExtract";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [state, setstate] = useState("");
+	const [output, setOutput] = useState(false);
+	const [notify, setNotify] = useState(false);
+	const [data, setData] = useState({});
+
+	const Submit = () => {
+		const smsBody = state;
+
+	
+
+		if (smsBody.length >= 112) {
+
+			const res = check(smsBody);
+
+			if (notify === true) {
+			return	setData(JSON.stringify(res));
+			}
+
+
+		
+			setData(JSON.stringify(res));
+			return setOutput(true);
+
+
+
+		}
+
+		return setNotify(true);
+	};
+
+	const handleOnChange = (e) => {
+		setstate(e.target.value);
+
+		if (e.target.value.length <= 112) {
+			setNotify(true);
+		}
+		setNotify(false);
+	};
+
+	return (
+		<div className="container">
+			<div className="bg-white p-4">
+				<div className="max-w-md">
+					<textarea
+						type="text"
+						placeholder="Copy and Paste message"
+						onChange={handleOnChange}
+						className="rounded-md border border-sky-500  w-full h-48 p-2 shadow-md "
+					/>
+				</div>
+
+				<div>{notify && <p className="py-2">Word count not enough</p>}</div>
+
+				<div className="py-2">
+					<button
+						disabled={!state ? true : false}
+						className={`rounded-md bg-indigo-500 w-auto text-sm px-4 py-2 text-base font-medium text-white inline-flex justify-center  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ${
+							!state ? `bg-slate-400` : `bg-indigo-500 hover:bg-indigo-700`
+						}`}
+						onClick={Submit}
+					>
+						Submit
+					</button>
+				</div>
+
+				<div className="mt-2 max-w-md">
+					{output && (
+						<div className="rounded-md border border-sky-500 w-full p-2">
+							<p className="text-sm text-gray-500 break-all ">{data}</p>
+
+							<div className="mt-2 flex justify-end content-end">
+								<button
+									className="rounded-md bg-red-500 w-auto text-sm px-4 py-2 text-base font-medium text-white inline-flex justify-center hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+									onClick={() => setOutput(false)}
+								>
+									Close
+								</button>
+							</div>
+						</div>
+					)}
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/smsExtract.js
@@ -0,0 +1,137 @@
+//regex
+const trxn_id_pattern = /Transaction I(d|D): \d+/gm;
+const amount_pattern = /GHS ?[0-9]+.[0-9]+?./gm || /GHS.?[0-9]+(.([0-9]+))/gi;
+const from_pattern =
+	/.[0-9]+.?from.?([a-z]+.?[a-z]+.?[a-z]+.?[a-z]+.?[a-z]+.?[a-z]+)/gim;
+const to_pattern = /\.[0-9]+.?to.?\-?\W?(\w+.?\w+.?\w+.?[A-Zz]+.?)?/gim;
+const reference_pattern = /Reference:.?\-?(\w+)?.?(\w+)?[0-9]*?-?([0-9]+)?/gim;
+const date_pattern = /at ([0-9]*\-[0-9]*\-[0-9]* )/gim;
+const time_pattern = /(([0-9]+\:[0-9]+\:[0-9]+)\.)/gim;
+
+const messsage_pattern =
+	/Message:Interest.?[a-z]+.?(\w+)?.(\w+)?.(\w+)?.([0-9]+)?/g;
+
+export const withdrawal = (data) => {
+	let amounts = data.match(amount_pattern);
+
+	if (amounts === null) {
+		return;
+	}
+
+	let trnx_id = data.match(trxn_id_pattern);
+	let withdrawal_amount = amounts[0];
+	let current_balance = amounts[1];
+	let fee_charged = amounts[2];
+	let payment_to = data.match(to_pattern);
+
+	return {
+		Withdrawal: data,
+
+		trnx_id: String(trnx_id[0]).slice(16),
+		withdrawal_amount: withdrawal_amount,
+		current_balance: current_balance,
+		fee_charged: fee_charged,
+		to: String(payment_to).slice(7),
+	};
+};
+
+export const receipt = (data) => {
+	let amounts = data.match(amount_pattern);
+
+	if (amounts === null) {
+		return;
+	}
+
+	let receipt_amount = amounts[0];
+	let current_balance = amounts[1];
+	let available_amount = amounts[2] || null;
+	let trnx_id = data.match(trxn_id_pattern);
+	let from = data.match(from_pattern) || null;
+	let message = data.match(messsage_pattern) ;
+
+	return {
+		Receipt: data,
+
+		receipt_amount: receipt_amount,
+		current_balance: current_balance,
+		available_amount: available_amount ,
+		trnx_id: String(trnx_id[0]).slice(16),
+		from: String(from).substring(9) ,
+		message: message ? String(message).substring(8) : null,
+	};
+};
+
+export const purchase = (data) => {
+	let amounts = data.match(amount_pattern);
+
+	if (amounts === null) {
+		return;
+	}
+
+	let purchase_amount = amounts[0];
+	let new_balance = amounts[1];
+	let fee_charged = amounts[2];
+	let trnx_id = data.match(trxn_id_pattern);
+	let payment_to = data.match(to_pattern);
+	let reference = data.match(reference_pattern);
+	let time = data.match(time_pattern) || null;
+	let date = data.match(date_pattern) || null;
+
+	return {
+		Purchase: data,
+
+		trnx_id: String(trnx_id[0]).slice(16),
+		purchase_amount: purchase_amount,
+		current_balance: new_balance,
+		fee_charged: fee_charged,
+		to: String(payment_to).slice(7),
+		reference: String(reference).substring(11),
+		time: String(time),
+		date: String(date).substring(3),
+	};
+};
+
+export const send = (data) => {
+	let amounts = data.match(amount_pattern);
+	let trnx_id = data.match(trxn_id_pattern);
+	let payment_to = data.match(to_pattern);
+	let reference = data.match(reference_pattern);
+	let time = data.match(time_pattern) || null;
+	let date = data.match(date_pattern) || null;
+	let send_amount = amounts[0];
+	let current_balance = amounts[1];
+	let available_balance = amounts[2];
+	let fee_charged = amounts[3];
+
+	return {
+		Send: data,
+
+		trnx_id: String(trnx_id[0]).slice(16),
+		send_amount: send_amount,
+		current_balance: current_balance,
+		available_balance: available_balance,
+		fee_charged: fee_charged,
+		reference: String(reference).substring(11),
+		time: String(time),
+		date: String(date).substring(3),
+		to: String(payment_to).includes("-")
+			? String(payment_to).slice(9)
+			: String(payment_to).slice(7),
+	};
+};
+
+export const check = (data) => {
+	if (data.includes("Cash Out")) {
+		return withdrawal(data);
+	}
+
+	if (data.includes("Payment received") || data.includes("An amount")) {
+		return receipt(data);
+	}
+
+	if (data.includes("Payment made") || data.includes("INTEROPERABILITY PUSH")) {
+		return send(data);
+	}
+
+	return purchase(data);
+};
