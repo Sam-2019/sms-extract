487b5432f062a20033712dd4be23edb12317d273
Samuel Martey <sam@encodevlabs.com>
2022-01-08 17:56:07 +0000

    added clear, renamed submit fns

--- src/App.js
+++ src/App.js
@@ -3,38 +3,34 @@ import { useState } from "react";
 import { check } from "./smsExtract";
 
 function App() {
-	const [state, setstate] = useState("");
+	const [state, setState] = useState("");
 	const [output, setOutput] = useState(false);
 	const [notify, setNotify] = useState(false);
 	const [data, setData] = useState({});
 
-	const Submit = () => {
+	const submit = () => {
 		const smsBody = state;
 
-	
-
 		if (smsBody.length >= 112) {
-
 			const res = check(smsBody);
 
 			if (notify === true) {
-			return	setData(JSON.stringify(res));
+				return setData(JSON.stringify(res));
 			}
 
-
-		
 			setData(JSON.stringify(res));
 			return setOutput(true);
-
-
-
 		}
 
 		return setNotify(true);
 	};
 
+	const clear = () => {
+		setState("");
+	};
+
 	const handleOnChange = (e) => {
-		setstate(e.target.value);
+		setState(e.target.value);
 
 		if (e.target.value.length <= 112) {
 			setNotify(true);
@@ -48,6 +44,7 @@ function App() {
 				<div className="max-w-md">
 					<textarea
 						type="text"
+						value={state}
 						placeholder="Copy and Paste message"
 						onChange={handleOnChange}
 						className="rounded-md border border-sky-500  w-full h-48 p-2 shadow-md "
@@ -62,9 +59,19 @@ function App() {
 						className={`rounded-md bg-indigo-500 w-auto text-sm px-4 py-2 text-base font-medium text-white inline-flex justify-center  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ${
 							!state ? `bg-slate-400` : `bg-indigo-500 hover:bg-indigo-700`
 						}`}
-						onClick={Submit}
+						onClick={submit}
 					>
 						Submit
+					</button>
+
+					<button
+						onClick={clear}
+						className={`rounded-md w-auto text-sm px-4 py-2 text-base font-medium  ${
+							!state ? `text-gray-500` : `text-indigo-900`
+						} inline-flex justify-center focus:outline-none`}
+						disabled={!state ? true : false}
+					>
+						Clear
 					</button>
 				</div>
 
@@ -75,6 +82,7 @@ function App() {
 
 							<div className="mt-2 flex justify-end content-end">
 								<button
+								disabled={!state ? true : false}
 									className="rounded-md bg-red-500 w-auto text-sm px-4 py-2 text-base font-medium text-white inline-flex justify-center hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
 									onClick={() => setOutput(false)}
 								>
