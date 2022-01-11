0f9fa32c51fd8b67df467e84b618ec71236362fd
Samuel Martey <sam@encodevlabs.com>
2022-01-11 20:53:41 +0000

    updateed withdrawal fn

--- src/smsExtract(vodafone).js
+++ src/smsExtract(vodafone).js
@@ -30,6 +30,38 @@ const reference_pattern = /Reference:.?[a-z]+/gi;
 
 const withdrawal = (data) => {
 	console.log({ Withdrawal: data });
+
+	let trnx_id = data.match(trxn_id_pattern);
+	let from_number = data.match(from_number_pattern);
+	let from_name = data.match(from_name_pattern);
+	let time = data.match(time_pattern);
+	let date = data.match(date_pattern);
+
+	let amounts = data.match(amount_pattern);
+	let withdrawal_amount = amounts ? amounts[0] : null;
+	let current_balance = amounts ? amounts[1] : null;
+
+	// console.log({ withdrawal_amount: withdrawal_amount });
+	// console.log({ current_balance: current_balance });
+	// console.log({ trnx_id: String(trnx_id[0]) });
+	// console.log({
+	// 	from_number: from_number ? String(from_number).substring(5) : null,
+	// });
+	// console.log({ from_name: from_name ? String(from_name).substring(3) : null });
+	// console.log({ time: time ? String(time) : null });
+	// console.log({ date: date ? String(date).trim() : null });
+
+	return {
+		Withdrawal: data,
+
+		withdrawal_amount: withdrawal_amount,
+		current_balance: current_balance,
+		trnx_id: String(trnx_id[0]),
+		from_number: from_number ? String(from_number).substring(5) : null,
+		from_name: from_name ? String(from_name).substring(3) : null,
+		time: time ? String(time) : null,
+		date: date ? String(date).trim() : null,
+	};
 };
 
 const receipt = (data) => {
