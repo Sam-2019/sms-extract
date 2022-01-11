b3b035af59bab3dca2ac71d742d726565157ce21
Samuel Martey <sam@encodevlabs.com>
2022-01-11 20:53:13 +0000

    added regex

--- src/smsExtract(vodafone).js
+++ src/smsExtract(vodafone).js
@@ -1,4 +1,32 @@
 //regex
+//eslint-disable-next-line
+const trxn_id_pattern = /(^[0-9]*)/gim;
+//eslint-disable-next-line
+const ref_no_pattern = /.?Ref\:?.?([0-9]+)/gm;
+//eslint-disable-next-line
+const amount_pattern = /GHS.?.?[0-9]+(.([0-9]+))/gi;
+//eslint-disable-next-line
+const from_name_pattern = / -.?[a-z]*.?[a-z]*.?[0-9]/gim;
+//eslint-disable-next-line
+const from_number_pattern = /(from).?[a-z]+(\d*)/gi;
+//eslint-disable-next-line
+const to_name_pattern = /(to).?(\d*) ((\w+).?(\w+)?.?(\w+)?)/;
+//eslint-disable-next-line
+const to_number_pattern = /(to).?(\d*)/gi;
+//eslint-disable-next-line
+const date_pattern = /([0-9]*\-[0-9]*\-[0-9]*).?/gim;
+//eslint-disable-next-line
+const time_pattern = /(([0-9]+\:[0-9]+\:[0-9]+))/gim;
+//eslint-disable-next-line
+const transfer_from = /Transfer From\:.?([0-9]+)-([0-9]+)/;
+//eslint-disable-next-line
+const transfer_name = /from.?([a-z]+).?([a-z]+)?.?([a-z]+)?/gi;
+//eslint-disable-next-line
+const for_number_pattern = /(for).?(\d*)/g;
+//eslint-disable-next-line
+const for_name_pattern = /(of).?(\w*)/g;
+//eslint-disable-next-line
+const reference_pattern = /Reference:.?[a-z]+/gi;
 
 const withdrawal = (data) => {
 	console.log({ Withdrawal: data });
